df1eaf7f98f1ea371b25ce78dd2fc7c02b9aa6ac
balakhtina <balakhtina@02PN-W-L06-005>
2024-01-29 11:40:19 +0100

    Nuovo modifica AndriiAggiungere i file di progetto.

---
+++ AndriiBalakhtin_Verifica/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AndriiBalakhtin_Verifica
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.txtBoxSearch = new System.Windows.Forms.TextBox();
+            this.BoxSearch = new System.Windows.Forms.GroupBox();
+            this.BoxElenco = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.BoxSearch.SuspendLayout();
+            this.BoxElenco.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(6, 22);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.Size = new System.Drawing.Size(689, 267);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellDoubleClick);
+            // 
+            // txtBoxSearch
+            // 
+            this.txtBoxSearch.Location = new System.Drawing.Point(6, 14);
+            this.txtBoxSearch.Name = "txtBoxSearch";
+            this.txtBoxSearch.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxSearch.TabIndex = 1;
+            this.txtBoxSearch.TextChanged += new System.EventHandler(this.txtBoxSearch_TextChanged);
+            // 
+            // BoxSearch
+            // 
+            this.BoxSearch.Controls.Add(this.txtBoxSearch);
+            this.BoxSearch.Location = new System.Drawing.Point(12, 83);
+            this.BoxSearch.Name = "BoxSearch";
+            this.BoxSearch.Size = new System.Drawing.Size(114, 43);
+            this.BoxSearch.TabIndex = 2;
+            this.BoxSearch.TabStop = false;
+            this.BoxSearch.Text = "Search";
+            // 
+            // BoxElenco
+            // 
+            this.BoxElenco.Controls.Add(this.dataGridView);
+            this.BoxElenco.Location = new System.Drawing.Point(12, 132);
+            this.BoxElenco.Name = "BoxElenco";
+            this.BoxElenco.Size = new System.Drawing.Size(702, 295);
+            this.BoxElenco.TabIndex = 3;
+            this.BoxElenco.TabStop = false;
+            this.BoxElenco.Text = "Elenco";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(719, 433);
+            this.Controls.Add(this.BoxElenco);
+            this.Controls.Add(this.BoxSearch);
+            this.Name = "Form1";
+            this.Text = "Andrii Balakhtin Verifica ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.BoxSearch.ResumeLayout(false);
+            this.BoxSearch.PerformLayout();
+            this.BoxElenco.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView;
+        private TextBox TextBoxSearch;
+        private TextBox txtBoxSearch;
+        private GroupBox BoxSearch;
+        private GroupBox BoxElenco;
+    }
+}
---
+++ AndriiBalakhtin_Verifica/Form2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AndriiBalakhtin_Verifica
+{
+    public partial class Form2 : Form
+    {
+        public string id_utente;
+        string connectionString = "server=127.0.0.1;database=magazzino;uid=root";
+
+        public Form2(string id)
+        {
+            InitializeComponent();
+            id_utente = id;
+        }
+        private void Connection()
+        {
+            MySqlConnection cnn = new MySqlConnection(connectionString);
+
+            try
+            {
+                cnn.Open();
+                string query = "SELECT * FROM elenco";
+
+                using (MySqlDataAdapter adpt = new MySqlDataAdapter(query, cnn))
+                {
+                    DataSet ds = new DataSet();
+                    adpt.Fill(ds);
+                //    dataGridView.DataSource = ds.Tables[0];
+                }
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnDeny_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connessione = new MySqlConnection(connectionString);
+            try
+            {
+                connessione.Open();
+
+                string query = $"UPDATE magazzino " +
+                    $"SET " +
+                    $"Descrizione = '{textBoxDescrizione.Text}', " +  
+                    $"WHERE  = {id_utente}";
+
+                MySqlCommand cmd = new MySqlCommand(query, connessione);
+                cmd.ExecuteNonQuery();
+
+                connessione.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Impossibile stabilire una connessione al DB");
+            }
+
+            this.Close();
+        }
+
+
+    }
+}
